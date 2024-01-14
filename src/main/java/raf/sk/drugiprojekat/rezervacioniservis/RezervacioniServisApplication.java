3039f56b8e6a6452049dd719a351ef7c2aeaf13a
doktor_mm <mmitreski8021rn@raf.rs>
2024-01-14 23:12:19 +0100

    Initial commit

---
+++ src/main/java/raf/sk/drugiprojekat/rezervacioniservis/RezervacioniServisApplication.java
@@ -0,0 +1,13 @@
+package raf.sk.drugiprojekat.rezervacioniservis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezervacioniServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezervacioniServisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raf/sk/drugiprojekat/rezervacioniservis/RezervacioniServisApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.sk.drugiprojekat.rezervacioniservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezervacioniServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
